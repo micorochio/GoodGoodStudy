c0bb4080fc231bb1a93008e47ba9ef4b9822d489
Zing <proling@126.com>
2017-05-16 00:12:38 +0800

    netty02

---
+++ netty-lecture/src/main/java/com/zing/netty/TestServer.java
@@ -0,0 +1,32 @@
+package com.zing.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by zing on 2017/5/11.
+ */
+public class TestServer {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();//获取连接
+        EventLoopGroup workerGroup = new NioEventLoopGroup();//对连接进行处理
+        try {
+
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();//用于启动服务端
+
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitialize());
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
---
+++ netty-lecture/src/main/java/com/zing/netty/TestServerInitialize.java
@@ -0,0 +1,21 @@
+package com.zing.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+
+/**
+ * Created by zing on 2017/5/15.
+ */
+public class TestServerInitialize extends ChannelInitializer<SocketChannel> {
+
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("httpServerCotec", new HttpServerCodec());
+        pipeline.addLast("textHttpServerHandler",new TestHttpServerHandler());
+    }
+}

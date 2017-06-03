3330ee9d11e17ff9f0b25ef7886312c8006f122a
Zing <proling@126.com>
2017-06-03 22:30:44 +0800

    Netty 聊天ok

--- netty-lecture/src/main/java/com/zing/netty/ssy/week02/chatroom/chatclient/ChatClient.java
+++ netty-lecture/src/main/java/com/zing/netty/ssy/week02/chatroom/chatclient/ChatClient.java
@@ -1,25 +1,35 @@
 package com.zing.netty.ssy.week02.chatroom.chatclient;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.*;
 
 /**
  * Created by zing on 2017/5/31.
  */
 public class ChatClient {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         EventLoopGroup chatGroup = new NioEventLoopGroup();
 
         try {
             Bootstrap chatClient = new Bootstrap();
-            chatClient.group(chatGroup).channel(NioServerSocketChannel.class)
+            chatClient.group(chatGroup).channel(NioSocketChannel.class)
                     .handler(new ChatClientInitializer());
 
-            ChannelFuture future = chatClient.connect("localhost", 8899).sync();
-            future.channel().closeFuture().sync();
+            Channel channel = chatClient.connect("localhost", 8899).channel();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            for (; ; ) {
+                channel.writeAndFlush(bufferedReader.readLine() + "\r\n");
+            }
+
+//            ChannelFuture future = chatClient.connect("localhost", 8899).sync();
+//            future.channel().closeFuture().sync();
 
         } finally {
             chatGroup.shutdownGracefully();
